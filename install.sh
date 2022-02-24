6465dee5c9824bbc6a69b3dfaf12543116bbffd4
Utkarsh <utkarsh.cpp@gmail.com>
2022-02-24 08:48:55 +0530

    Run this to INSTALL the ilk-toolkit

--- install.sh
+++ install.sh
@@ -1,32 +1,19 @@
-#Checking Terminal App name
-termname=`echo $0`
+#Name of Current User
+curruser=`whoami`
 
-#Installing Nmap from Source Code
+#Installing Nmap
 bzip2 -cd nmap-7.92.tar.bz2 | tar xvf -
 cd nmap-7.92
-./configure
-make
-make install
+sudo ./configure
+sudo make
+sudo make install
 cd ..
 
 #Installing Nikto
 git clone https://github.com/sullo/nikto.git
-cp ~/ilk-toolkit/nikto /opt/
-if [[ $termname -eq 'bash' || $termname -eq '-bash' || $termname -eq '/usr/bin/bash' || $termname -eq '/bin/bash']
-then
-    echo "export PATH=$PATH:/opt/nikto/program" >> ~/.bashrc
-    echo "alias nikto='perl /opt/nikto/program/nikto.pl" >> ~/.bashrc
-    source ~/.bashrc
-elif [[ $termname -eq 'zsh' || $termname -eq '-zsh' || $termname -eq '/usr/bin/zsh' || $termname -eq '/bin/zsh']
-then
-    echo "export PATH=$PATH:/opt/nikto/program" >> ~/.zshrc
-    echo "alias nikto='perl /opt/nikto/program/nikto.pl" >> ~/.zshrc
-    source ~/.zshrc
-else
-    echo "export PATH=$PATH:/opt/nikto/program" >> ~/.kshrc
-    echo "alias nikto='perl /opt/nikto/program/nikto.pl" >> ~/.kshrc
-    source ~/.kshrc
-fi
+sudo cp -r /home/$curruser/ilk-toolkit/nikto /opt/
+cat  forbashrc.txt >> /home/$curruser/.bashrc
+cat  forbashrc.txt >> /home/$curruser/.bashrc
 
 #Installing BurpSuite Community Edition
 chmod +x burpsuite_community_linux_v2022_1_1.sh 
